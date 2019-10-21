fcb068f323d3f05443bf09d85a136ae81f6e3b94
vktor <victor.carranz@hotmail.com>
2019-10-20 18:19:07 -0600

    show first FormFragment

--- app/src/main/java/com/example/exampleclass1part2/Fragment_SuccessView.kt
+++ app/src/main/java/com/example/exampleclass1part2/Fragment_SuccessView.kt
@@ -10,97 +10,32 @@ import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.fragment_form_view.*
+import java.lang.RuntimeException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SuccessView.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SuccessView.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SuccessView : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //spinner_hownotice.adapter = ArrayAdapter<String>(this.activity!!.baseContext, R.layout.spinner_item)
-       // var adapter = ArrayAdapter.createFromResource(activity!!.applicationContext,R.array.form_spinner, R.layout.spinner_item)
-       // spinner_hownotice.adapter
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var listenerSuccessView: OnSuccessFragmentInteractionListener? =null
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_success_view, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return LayoutInflater.from(container?.context).inflate(R.layout.fragment_success_view, container, false)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        if(context is OnSuccessFragmentInteractionListener){
+            listenerSuccessView = context
+        }else{
+            throw RuntimeException(context.toString() + "implementar listener")
         }
     }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
+    fun onButtonPressed(uri: Uri){
+        listenerSuccessView?.onSendClickInteraction()
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SuccessView.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SuccessView().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    interface OnSuccessFragmentInteractionListener{
+        fun onSendClickInteraction()
     }
 }
--- app/src/main/java/com/example/exampleclass1part2/MainActivity.kt
+++ app/src/main/java/com/example/exampleclass1part2/MainActivity.kt
@@ -2,20 +2,21 @@ package com.example.exampleclass1part2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SuccessView.OnSuccessFragmentInteractionListener {
+    override fun onSendClickInteraction() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // loadForm(formulario())
+        loadForm(FragmentFormView())
     }
-    private fun loadForm(fr:formulario){
+    private fun loadForm(fr:FragmentFormView){
         val formu: FragmentTransaction = supportFragmentManager.beginTransaction()
         formu.replace(R.id.fragment, fr)
-        formu.addToBackStack(null)
         formu.commit()
     }
 }
